e548e5b1b9127f152cbc11ed51fd43c2afd43f11
KevinFampou <fampoukevin3@gmail.com>
2022-12-01 16:16:40 +0100

    page d'accueil

---
+++ front/js/script.js
@@ -0,0 +1,28 @@
+let kanapData = []
+const items = document.getElementById("items")
+
+const  fetchKanap = async () => {
+  await fetch("http://localhost:3000/api/products")
+        .then((res) => res.json())
+        .then((data) => kanapData = data)
+                                                    
+}
+
+const kanapDisplay = async () => {
+    await fetchKanap() 
+
+    kanapData.forEach(element => {
+        items.innerHTML += 
+        `<div>
+            <a href="./product.html?_id=${element._id}">
+                <article>
+                    <img src="${element.imageUrl}" alt="${element.altTxt}">
+                    <h3 class="productName">${element.name}</h3>
+                    <p class="productDescription">${element.description}</p>
+                </article>
+            </a>
+        </div>`
+    });
+}
+
+kanapDisplay()
